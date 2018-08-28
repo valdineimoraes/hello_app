c69fd2345fe141b72cf38f401d370a41e3891ece
Valdinei Moraes <valdineimoraes@alunos.utfpr.edu.br>
2018-08-28 14:44:19 -0300

    new route goodbye

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,11 @@ class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
 
     def hello
-        render html: "hola, mundo!"
+        render html: " ¡Hola, mundo!"
+    end
+
+    def goodbye
+        render html: "goodbye , world!"
     end
 
 end
